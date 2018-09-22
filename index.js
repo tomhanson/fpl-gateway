2d667f92465f7d59b2e2a22a176a953c61b4e4ae
Tom Hanson <tomhanson1985@gmail.com>
2018-09-22 18:02:32 +0100

    feat: split the code up a little to make it more manageable(WIP)

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const { ApolloServer, gql } = require('apollo-server');
 const fetch = require('node-fetch');
 const { importSchema } = require('graphql-import');
 
+const typeResolvers = require('./resolvers/typeResolvers');
+const query = require('./resolvers/QueryResolver');
+
 const typeDefs = importSchema('./schema/app.graphql');
 
 function sortPlayers(sort, sortOrder) {
@@ -41,63 +44,20 @@ const resolvers = {
 
         const updatedPage = page - 1;
         return sortedData.slice((updatedPage * per_page), (per_page * page));
-        // return data.elements
       }
       catch (err) {
 
       }
     },
   },
-  SortOption: {
-    firstName: 'first_name',
-    lastName: 'second_name',
-    id: 'id',
-    squadNumber: 'squad_number',
-    chanceOfPlayingThisRound: 'chance_of_playing_this_round',
-    chanceOfPlayingNextRound: 'chance_of_playing_next_round',
-    totalTransfersOut: 'transfers_out',
-    totalTransfersIn: 'transfers_in',
-    percentSelected: 'selected_by_percent',
-    totalPoints: 'total_points',
-    totalMinutesPlayed: 'minutes',
-    yellowCards: 'yellow_cards',
-    redCards: 'red_cards',
-    penaltiesSaved: 'penalties_saved',
-    penaltiesMissed: 'penalties_missed',
-    cost: 'now_cost',
-    timesInDreamTeam: 'dreamteam_count',
-    inCurrentDreamTeam: 'in_dreamteam',
-    averagePointsPerGame: 'points_per_game',
-    goals: 'goals_scored',
-    cleanSheets: 'clean_sheets',
-    goalsConceded: 'goals_conceded'
-  },
-  playerData: {
-    firstName: ({ first_name }) => first_name,
-    lastName: ({ second_name }) => second_name,
-    id: ({ id }) => id,
-    squadNumber: ({ squad_number }) => squad_number,
-    chanceOfPlayingThisRound: ({ chance_of_playing_this_round }) => chance_of_playing_this_round,
-    chanceOfPlayingNextRound: ({ chance_of_playing_next_round }) => chance_of_playing_next_round,
-    totalTransfersOut: ({ transfers_out }) => transfers_out,
-    totalTransfersIn: ({ transfers_in }) => transfers_in,
-    percentSelected: ({ selected_by_percent }) => selected_by_percent,
-    totalPoints: ({ total_points }) => total_points,
-    totalMinutesPlayed: ({ minutes }) => minutes,
-    yellowCards: ({ yellow_cards }) => yellow_cards,
-    redCards: ({ red_cards }) => red_cards,
-    penaltiesSaved: ({ penalties_saved }) => penalties_saved,
-    penaltiesMissed: ({ penalties_missed }) => penalties_missed,
-    cost: ({ now_cost }) => (now_cost / 10).toFixed(1),
-    timesInDreamTeam: ({ dreamteam_count }) => dreamteam_count,
-    inCurrentDreamTeam: ({ in_dreamteam }) => in_dreamteam,
-    averagePointsPerGame: ({ points_per_game }) => points_per_game,
-    goals: ({ goals_scored }) => goals_scored,
-    cleanSheets: ({ clean_sheets }) => clean_sheets,
-    goalsConceded: ({ goals_conceded }) => goals_conceded
-  }
+  ...typeResolvers
 
 
+};
+//TOD why doesnt this work?
+const resolvers2 = {
+  Query: query,
+  ...typeResolvers
 };
 
 // In the most basic sense, the ApolloServer can be started
---
+++ resolvers/QueryResolver.js
@@ -0,0 +1,47 @@
+var fetch = require('node-fetch');
+
+var query = module.exports = {
+  async getPlayers(_, { per_page, page, sort, sortOrder, filter }) {
+
+    try {
+
+      const data = await fetch(`https://fantasy.premierleague.com/drf/bootstrap-static`).then(data => data.json());
+
+
+      const sortedData = sort ? data.elements.sort(sortPlayers(sort, sortOrder)) : data.elements;
+
+      const updatedPage = page - 1;
+      return sortedData.slice((updatedPage * per_page), (per_page * page));
+      // return data.elements
+    }
+    catch (err) {
+
+    }
+  },
+
+}
+
+function sortPlayers(sort, sortOrder) {
+  return function (a, b) {
+    console.log(sort)
+    const optionA = typeof a[sort] === 'number' ? a[sort] : a[sort].toUpperCase();
+    const optionB = typeof b[sort] === 'number' ? b[sort] : b[sort].toUpperCase();
+    if (sortOrder === 'ASC') {
+      if (optionA < optionB) {
+        return -1;
+      }
+      if (optionA > optionB) {
+        return 1;
+      }
+    } else {
+      if (optionA < optionB) {
+        return 1;
+      }
+      if (optionA > optionB) {
+        return -1;
+      }
+    }
+    // equal 
+    return 0;
+  }
+}
