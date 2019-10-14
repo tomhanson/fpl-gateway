14c2bc5dfa06efb59e3b003b663723e198fb7a13
Gary Hanks <gary.d.hanks@gmail.com>
2019-10-14 13:04:19 +0100

    remove console

--- src/resolvers/typeResolvers/PlayerPositionData.js
+++ src/resolvers/typeResolvers/PlayerPositionData.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 const PlayerPositionData = {
   id: ({ id }) => id,
-  singularName: ({ singular_name }) => console.log('singular_name', singular_name) || singular_name,
+  singularName: ({ singular_name }) => singular_name,
   shortName: ({ singular_name_short }) => singular_name_short
 };
 
